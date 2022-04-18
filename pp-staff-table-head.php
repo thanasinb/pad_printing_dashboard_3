2d7a329088d1bb1e0972c8bc68dc92d52a0aa922
thanasinb <thanasin.b@gmail.com>
2022-04-18 17:43:52 +0700

    Add 'Prefix' table head

--- pp-staff-table-head.php
+++ pp-staff-table-head.php
@@ -3,6 +3,7 @@
 <tr class="fw-bold">
     <th class="text-center">Staff ID</th>
     <th class="text-center">RFID</th>
+    <th class="text-center">Prefix</th>
     <th class="text-center">Name</th>
     <th class="text-center">Role</th>
     <th class="text-center">Shif</th>

984c81808e5af241e31165459a5f0ce57bfe0942
Krisada-Kareeso <zerkid.playstore@gmail.com>
2022-04-19 16:07:46 +0700

    remove ajax test

--- ajax/pp-staff-load.php
+++ ajax/pp-staff-load.php
@@ -3,7 +3,7 @@ require '../update/establish.php';
 
 //SELECT staff.id_staff, staff.id_rfid, prefix.prefix, staff.name_first, staff.name_last, staff.site, role.role, staff.id_shif FROM staff INNER JOIN role ON staff.id_role=role.id_role INNER JOIN prefix ON staff.prefix=prefix.id_prefix WHERE id_staff='0009'
 
-$sql = "SELECT staff.id_staff, staff.id_rfid, prefix.prefix, staff.name_first, staff.name_last, staff.site, role.role, staff.id_shif FROM staff INNER JOIN role ON staff.id_role=role.id_role INNER JOIN prefix ON staff.prefix=prefix.id_prefix WHERE id_staff='" . $_GET['id_staff'] . "'";
+$sql = "SELECT staff.id_staff, staff.id_rfid, prefix.prefix, staff.site, role.role, staff.id_shif FROM staff INNER JOIN role ON staff.id_role=role.id_role INNER JOIN prefix ON staff.prefix=prefix.id_prefix WHERE id_staff='" . $_GET['id_staff'] . "'";
 
 $result = $conn->query($sql);
 $db_staff = $result->fetch_assoc();

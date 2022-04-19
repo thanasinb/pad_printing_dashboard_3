44bbd63c19b2c90c36422a37f7f22ced205b2eb2
Krisada-Kareeso <zerkid.playstore@gmail.com>
2022-04-19 22:55:58 +0700

    test

--- pp-staff-list-staff-script-operator.php
+++ pp-staff-list-staff-script-operator.php
@@ -19,7 +19,7 @@ while($data_staff = $result_staff->fetch_assoc()) {
         echo "นาง";
     elseif ($prefix==3)
         echo "นางสาว";
-    echo "<td>". $data_staff['name_first'] ."      ". $data_staff['name_last']."</td>";
+    echo "<td>". $data_staff['name_first'] ." ". $data_staff['name_last']."</td>";
 
     echo "<td class='role'>";
     $id_role = intval($data_staff['id_role']);

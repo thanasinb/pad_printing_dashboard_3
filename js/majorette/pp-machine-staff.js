f49ee46397d8141019943196383a9f34cd65a632
Krisada-Kareeso <zerkid.playstore@gmail.com>
2022-04-19 16:58:37 +0700

    test

--- js/majorette/pp-machine-staff.js
+++ js/majorette/pp-machine-staff.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         $('#shift').prop('disabled', true);
         $('#role').prop('disabled', true);
         $('#input_site').prop('disabled', true);
-        $('#button_save_rfid').show();
+        $('#button_save_rfid').hide();
         $('#button_rfid').show();
     });
 
@@ -86,8 +86,8 @@ $(document).ready(function(){
         var id_staff = $(this).parent().parent().find('.id_staff').html();
         var id_rfid = $(this).parent().parent().find('.rfid').html();
         var prefix = $(this).parent().parent().find('.prefix').html();
-        //var name_first = $(this).parent().parent().find('.name_first').html();
-        //var name_last = $(this).parent().parent().find('.name_last').html();
+        var name_first = $(this).parent().parent().find('.name_first').html();
+        var name_last = $(this).parent().parent().find('.name_last').html();
         var role = $(this).parent().parent().find('.role').html();
         var shif = $(this).parent().parent().find('.shif').html();
 
@@ -126,34 +126,34 @@ $(document).ready(function(){
         $('#input_staff_id').val(id_staff);
         $('#input_rfid').val(id_rfid);
         $('#prefix_name').val(prefix_val);
-        //$('#input_name').val(name_first);
-        //$('#input_last').val(name_last);
+        $('#input_name').val(name_first);
+        $('#input_last').val(name_last);
         $('#role').val(role_val);
         $('#shift').val(shif);
 
         $('#staff_modal').modal('show');
         // $('#modal_span_staff_id').text(id_staff);
-        $.ajax({
-         url: "ajax/pp-staff-load.php",
-        type: "GET",
-        data: {
-         id_staff: id_staff
-         },
-        context: this,
-        cache: false,
-        success: function(dataResult){
-        // alert(dataResult);
-         var dataResult = JSON.parse(dataResult);
-        // $('#input_staff_id').text(dataResult.id_staff);
-        //$('#input_rfid').val(dataResult.id_rfid);
-        //$('#modal_prefix').text(dataResult.prefix);
-         $('#input_name').val(dataResult.name_first);
-        $('#input_last').val(dataResult.name_last);
-        $('#input_site').val(dataResult.site);
-        // $('#modal_role').text(dataResult.role);
-        //$('#modal_shif').text(dataResult.id_shif);
-         }
-        });
+        // $.ajax({
+        //  url: "ajax/pp-staff-load.php",
+        // type: "GET",
+        // data: {
+        //  id_staff: id_staff
+        //  },
+        // context: this,
+        // cache: false,
+        // success: function(dataResult){
+        // // alert(dataResult);
+        //  var dataResult = JSON.parse(dataResult);
+        // // $('#input_staff_id').text(dataResult.id_staff);
+        // //$('#input_rfid').val(dataResult.id_rfid);
+        // //$('#modal_prefix').text(dataResult.prefix);
+        //  $('#input_name').val(dataResult.name_first);
+        // $('#input_last').val(dataResult.name_last);
+        // $('#input_site').val(dataResult.site);
+        // // $('#modal_role').text(dataResult.role);
+        // //$('#modal_shif').text(dataResult.id_shif);
+        //  }
+        // });
     });
 
     $('.staff_delete').click(function (){

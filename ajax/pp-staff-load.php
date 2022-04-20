35fa6726c2ed283df076e067ecadeb1f4e3723ef
Krisada-Kareeso <zerkid.playstore@gmail.com>
2022-04-20 09:23:41 +0700

    test

--- ajax/pp-staff-load.php
+++ ajax/pp-staff-load.php
@@ -3,15 +3,10 @@ require '../update/establish.php';
 
 //SELECT staff.id_staff, staff.id_rfid, prefix.prefix, staff.name_first, staff.name_last, staff.site, role.role, staff.id_shif FROM staff INNER JOIN role ON staff.id_role=role.id_role INNER JOIN prefix ON staff.prefix=prefix.id_prefix WHERE id_staff='0009'
 
-//$sql = "SELECT staff.id_staff, staff.id_rfid, prefix.prefix, staff.name_first, staff.name_last, staff.site, role.role, staff.id_shif FROM staff INNER JOIN role ON staff.id_role=role.id_role INNER JOIN prefix ON staff.prefix=prefix.id_prefix WHERE id_staff='" . $_GET['id_staff'] . "'";
+$sql = "SELECT staff.id_staff, staff.id_rfid, prefix.prefix, staff.name_first, staff.name_last, staff.site, role.role, staff.id_shif FROM staff INNER JOIN role ON staff.id_role=role.id_role INNER JOIN prefix ON staff.prefix=prefix.id_prefix WHERE id_staff='" . $_GET['id_staff'] . "'";
 
-//$result = $conn->query($sql);
-//$db_staff = $result->fetch_assoc();
-
-<?php require "pp-staff-list-staff-script-operator.php"; ?>
-
-$n_first = $data_staff['name_first'];
-$n_last = $data_staff['name_last'];
+$result = $conn->query($sql);
+$db_staff = $result->fetch_assoc();
 
 echo json_encode($db_staff, JSON_UNESCAPED_UNICODE);
 

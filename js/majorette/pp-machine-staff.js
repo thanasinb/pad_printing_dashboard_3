57e8c2f05072e93cce104bbd707559aa18b5f016
Krisada-Kareeso <zerkid.playstore@gmail.com>
2022-04-22 10:26:30 +0700

    specialcarcheck

--- js/majorette/pp-machine-staff.js
+++ js/majorette/pp-machine-staff.js
@@ -27,6 +27,7 @@ $(document).ready(function(){
         var shift= $('#shift').val();
         var site= $('#input_site').val();
 
+        //specialcarcheck
         if(specialchar.test(name+last)) {
             alert("Name must not contain special characters");
         }

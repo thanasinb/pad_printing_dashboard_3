7f51de8db94c81f1d904d84d55deeb23f51bc190
ThitipongPhasom <titipong.k5143@gmail.com>
2022-05-05 16:12:05 +0700

    Edit Employee Code Change Code id_staff

--- update/repeat.php
+++ update/repeat.php
@@ -7,8 +7,9 @@ $sql = $sql . $data_activity["id_activity"] . ", ";
 $sql = $sql . "CURRENT_TIMESTAMP(), ";
 $sql = $sql . "1)";
 $result = $conn->query($sql);
-$sql = "UPDATE activity SET id_activity = num_repeat + 1 WHERE id_task=" . $_GET["id_task"] . " AND id_machine='" . $_GET["id_mc"] ."' AND status_work = 1";
-//$sql = "UPDATE activity SET num_repeat = num_repeat + 1 WHERE id_task = 2395 AND id_machine = '02-00' AND status_work = 1";
+
+$sql = "UPDATE activity SET num_repeat = num_repeat + 1 WHERE id_activity='" . $data_activity["id_activity"] . "' AND id_machine ='".$_GET["id_mc"] . "'";
+$data_repeat = $conn->query($sql);
 
 require 'terminate.php';
 ?>

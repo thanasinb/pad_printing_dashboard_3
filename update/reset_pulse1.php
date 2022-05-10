00935af1a746d1852e140ff2cd7602b0ffee935e
sroysawan <sroysawan.kcd@gmail.com>
2022-05-10 09:48:33 +0700

    reset no_pulse1

--- update/reset_pulse1.php
+++ update/reset_pulse1.php
@@ -3,12 +3,14 @@ require 'establish.php';
 
 $sql = "SELECT id_task, id_staff FROM machine_queue WHERE id_machine='".$_GET['id_mc']."' AND queue_number=1";
 $result = $conn->query($sql);
-$reset_pulse1 = $result->fetch_assoc();
+$data_pulse1 = $result->fetch_assoc();
 
 $sql = "UPDATE activity SET no_pulse1=0  WHERE id_task='" . $_GET["id_task"] . "'AND id_staff='" . $_GET["id_staff"] . "' AND status_work=1";
-$data_reset = $conn->query($sql);
+$reset_pulse1 = $conn->query($sql);
+//$data_reset_pulse1 = $data_reset->fetch_assoc();
 
 echo "RESET NO_PULSE1 COMPLETE";
+//echo $reset_pulse1;
 
 require 'terminate.php';
 ?>
